f4cb894cb04311d18fadb4e75091f63f5ebe8c12
Luis Cacho <lcacho@redhat.com>
2024-04-12 10:50:16 -0500

    TXLF - Test automatic promotion

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ import (
 )
 
 func main() {
-	myFigure := figure.NewColorFigure("luiscachog is Awesome!!!", "larry3d", "yellow", true)
+	myFigure := figure.NewColorFigure("luiscachog is Awesome and great!!!", "larry3d", "yellow", true)
 	myFigure.Print()
 
 	if secret := os.Getenv("SECRET"); secret != "" {
 		mySecretFigure := figure.NewColorFigure(fmt.Sprintf("Secret value is: %s", secret), "larry3d", "red", true)
 		mySecretFigure.Print()
 	}
-	
+
 	time.Sleep(10 * time.Hour)
 }
